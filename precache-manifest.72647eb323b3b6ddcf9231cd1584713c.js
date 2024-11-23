af105f9cd3addf454917d2355d8f5adb88f1cb81
Hotdoog1234 <robert_francis@shieldmw.com>
2024-11-23 10:04:06 -0500

    Updates

---
+++ precache-manifest.72647eb323b3b6ddcf9231cd1584713c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd4715e8371f6a8e55b6ad3394e529b",
+    "url": "/preservation-guide/index.html"
+  },
+  {
+    "revision": "c4634b8861f2df2ac160",
+    "url": "/preservation-guide/static/css/main.6bcc961d.chunk.css"
+  },
+  {
+    "revision": "f18e9280047d2c2f67b5",
+    "url": "/preservation-guide/static/js/2.afef58d2.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/preservation-guide/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c4634b8861f2df2ac160",
+    "url": "/preservation-guide/static/js/main.30d847b8.chunk.js"
+  },
+  {
+    "revision": "557f1311f37a32b14b95",
+    "url": "/preservation-guide/static/js/runtime~main.d674bcdd.js"
+  }
+]);
